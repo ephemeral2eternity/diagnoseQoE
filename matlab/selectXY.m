c4b3e2bdf0e967e2eae6840ca4fb167a6fc6b63b
ssfffss <senbof@gmail.com>
2016-02-27 00:41:31 +0000

    lasso regression

--- matlab/selectXY.m
+++ matlab/selectXY.m
@@ -7,4 +7,24 @@ load('QoE');
 TS=TS-mod(TS,1);
 [value,xindex]=intersect(metricsTime,TS);
 X=metrics(xindex,:);
-[Y,index]=intersect(TS, value);
+[yvalue, yindex]=intersect(TS, value);
+Y=responseTime(yindex)';
+
+t=100;
+B=LassoActiveSet(X,Y,t);
+
+figure;
+plot(Y-X*B, 'r-');
+ylabel('testing error');
+xlabel('time');
+figfile=sprintf('error');
+print(figfile,'-dpng','-r800');
+
+figure;
+plot(Y, 'g*-');hold on;
+plot(X*B, 'bo-');
+legend('real', 'estimated');
+ylabel('response time');
+xlabel('time');
+figfile=sprintf('compare');
+print(figfile,'-dpng','-r800');
